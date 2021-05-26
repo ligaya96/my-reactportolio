8de577ab42d2f3e9e2e514e5bec4ab0f31479ebf
Ligaya <ligaya96@gmail.com>
2021-05-25 23:48:22 -0700

    fixing small bugs

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -10,7 +10,7 @@ function About()  {
         <div className = "row">
             <div className = "img">
              {/* picture of me */} 
-             <img src="../public/images/professional.JPG" alt="professional"  class="img-thumbnail" /> 
+             <img src="../public/images/professional.jpg" alt="professional"  class="img-thumbnail" /> 
             </div>
             {/* A paragraph about me */}
             <p className="text"> 
